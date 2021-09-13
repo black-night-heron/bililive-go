eadfd65791958a0320218a449dcb2d975af12042
GAsss <14975428+Leo-GAsss@users.noreply.github.com>
2021-09-13 12:13:26 +0800

    only build needed architectures

--- src/hack/release.sh
+++ src/hack/release.sh
@@ -30,14 +30,7 @@ package() {
   echo $BIN_PATH/$res
 }
 
-for dist in $(go tool dist list); do
-  case $dist in
-  linux/loong64 | android/* | ios/* | js/wasm )
-    continue
-    ;;
-  *) ;;
-
-  esac
+for dist in linux/amd64 linux/arm64 windows/amd64; do
   platform=$(echo ${dist} | cut -d'/' -f1)
   arch=$(echo ${dist} | cut -d'/' -f2)
   make PLATFORM=${platform} ARCH=${arch} bililive
